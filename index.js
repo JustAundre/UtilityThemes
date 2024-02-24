32337d0bec6a4f619cf50444ab07842b0ee75956
ItsAundre! <126304209+ItsAundre@users.noreply.github.com>
2024-02-23 18:59:25 -0600

    Update

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
+// Autoplay the background
 document.getElementById('background').play();
+
+function setRandomHue() {
+    // Generate random number between 0 and 360
+    const randomAngle = Math.floor(Math.random() * 361);
+  
+    // Append "deg" and create CSS variable string
+    const hueValue = `${randomAngle}deg`;
+  
+    // Set the CSS variable "--hue"
+    document.documentElement.style.setProperty('--hue', hueValue);
+  }
+  
+  // Call the function initially to set the variable
+  setRandomHue();
