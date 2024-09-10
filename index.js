532e87019e2316b950af7ca45b887d21853a745e
JustAundre <126304209+JustAundre@users.noreply.github.com>
2024-09-10 18:07:29 -0500

    CSS consistency changes Javascript code changes

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 function generateRandomValues() {
-  const x1 = Math.floor(Math.random() * 201) - 100;
-  const x2 = Math.floor(Math.random() * 201) - 100;
-  const x3 = Math.floor(Math.random() * 201) - 100;
-  const y1 = Math.floor(Math.random() * 101) - 50;
-  const y2 = Math.floor(Math.random() * 101) - 50;
-  const y3 = Math.floor(Math.random() * 101) - 50;
+  const x1 = Math.round(Math.random() * 75) - 37.5;
+  const x2 = Math.round(Math.random() * 64) - 32;
+  const x3 = Math.round(Math.random() * 56) - 28;
+  const y1 = Math.round(Math.random() * 70) - 35;
+  const y2 = Math.round(Math.random() * 57) - 28.5;
+  const y3 = Math.round(Math.random() * 50) - 25;
 
   document.documentElement.style.setProperty('--x1', `${x1}%`);
   document.documentElement.style.setProperty('--x2', `${x2}%`);
